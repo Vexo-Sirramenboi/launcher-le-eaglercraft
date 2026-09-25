0ca2f1af0a405955f04c49d15477913eb38585df
Vexo Sirramenboi <SirramenboiEmail@proton.me>
2026-09-24 22:47:17 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,42 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker Installed');
+const CACHE_NAME = "launcher-le-eaglercraft-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./favicon.ico",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
